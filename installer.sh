a0fbc86bdff5eb42a04d73cfb2a3da8116f796ea
magocarlos99 <magocarlos1999@gmail.com>
2015-12-16 14:53:40 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -66,6 +66,7 @@ sudo apt-get -y --force-yes install spotify-client
 sudo apt-get -y --force-yes install libgtk-3-dev
 sudo apt-get -y --force-yes install kazam
 sudo apt-get -y --force-yes install gimp
+sudo apt-get -y --force-yes install aircrack-ng
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 ~/.dropbox-dist/dropboxd
 wget http://cz.archive.ubuntu.com/ubuntu/pool/main/libg/libgcrypt11/libgcrypt11_1.5.3-2ubuntu4.2_amd64.deb
